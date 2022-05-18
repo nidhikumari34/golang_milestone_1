489d1c8383d5f4c4201e8887cdc4ea86a346b372
Nidhi Kumari <nidhikumari34@deloitte.com>
2022-05-18 10:14:31 +0530

    modified main

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/csv"
-	"fmt"
 	"log"
 	"os"
-
-	_ "github.com/go-sql-driver/mysql"
 )
-
-type netflixshows struct {
-	show_id      string
-	show_type    string
-	title        string
-	director     string
-	cast         string
-	country      string
-	date_added   string
-	release_year string
-	rating       string
-	duration     string
-	listed_in    string
-	description  string
-}
 
 func main() {
 	csvFile, err := os.Open("netflix_titles.csv")
@@ -39,21 +20,7 @@ func main() {
 	}
 
 	netflix_titles = sort_csv(netflix_titles)
-
-	db, err := sql.Open("mysql", "root:<knightrider@6N>@tcp(127.0.0.1:3306)/test")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer db.Close()
-	insert, err := db.Query("INSERT INTO netflix_show_details VALUES('1')")
-	if err != nil {
-		panic(err.Error())
-	}
-	defer insert.Close()
-	fmt.Println("Success!")
-
-	//insert_db(db, netflixshows)
-	//tv_shows(netflix_titles)
-	//horror_movies(netflix_titles)
-	//indian_movies(netflix_titles)
+	tv_shows(netflix_titles)
+	horror_movies(netflix_titles)
+	indian_movies(netflix_titles)
 }
